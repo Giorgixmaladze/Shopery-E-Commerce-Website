c08a43177cdea366c8c528626d54eebc4138587d
Giorgixmaladze <xmaladzegiorgi2022@gmail.com>
2025-05-06 02:53:18 +0400

    commit

--- src/Components/navigation/NavigationBody.jsx
+++ src/Components/navigation/NavigationBody.jsx
@@ -7,16 +7,16 @@ const NavigationBody = () =>{
                 <h1 className="text-3xl ">Ecobazar</h1>
             </span>
             <form className="flex items-center">
-                <input type="text" className="bg-[url('public/navigation/Search.png')] bg-[20px] bg-no-repeat border pl-[50px]  border-gray-300 rounded-[7px] w-100 h-[45px]" name="search" placeholder="Search"/>
-                <button className="w-[98px] h-[45px] bg-searchGreen">Search</button>
+                <input type="text" className="bg-[url('public/navigation/Search.png')] bg-[20px] bg-no-repeat border pl-[50px]  border-gray-300 rounded-tl-[10px] rounded-bl-[10px] outline-0 w-100 h-[45px]" name="search" placeholder="Search"/>
+                <button className="w-[98px] h-[45px] bg-mwvane rounded-tr-[10px] rounded-br-[10px]">Search</button>
             </form>
-            <span>
+            <span className="flex w-[191px] h-[34px] gap-[25px] items-center ">
                 <img src="public/navigation/Heart.png" alt="Heart" />
-                <div className="w-0 h-5 border border-collapse-collapse border-gray-400"></div>
+                <div className="w-0 h-[34px] border border-collapse-collapse border-gray-400"></div>
                 <img src="public/navigation/wishlist.png" alt="" />
-                <span>
-                    <p>Shopping cart:</p>
-                    <h3>0</h3>
+                <span className="w-[81px] h-[34px]">
+                    <p className="text-[10px] w-[65px] text-gray-700">Shopping cart:</p>
+                    <h3 className="text-sm">0</h3>
                 </span>
             </span>
         </div>
